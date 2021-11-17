02606cbd1d38d8d88fa89fa2781d3bf1eaabf412
aishwarya <aishwaryakrhebbar@gmail.com>
2021-11-17 10:56:53 +0530

    first commit

---
+++ LeapYear.js
@@ -0,0 +1,12 @@
+function leapyear(year)
+{
+    if((year % 100 != 0) && (year % 4 == 0)||(year % 400 == 0)){
+        console.log(year +" is a leap year");
+    }
+    else{
+        console.log(year + "is not a leap year");
+    }
+
+}
+const year = prompt("Enter a year:");
+leapyear(year)
